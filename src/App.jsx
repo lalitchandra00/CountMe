c0a067d20f3a796b3f0a6033bd97e38976fb1d03
lalitchandra00 <mail4lalitchandra@gmail.com>
2026-06-23 00:20:26 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import './App.css'
 
 
 function App() {
+  const [counter, SetCounter] = useState(0);
+  //use state returns value in array format variable counter(here) which stores value and another method setcounter (here) which is use to display the value in UI
 
   let count = 5
   function IncValue(){
